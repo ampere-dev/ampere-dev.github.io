7d3276270ffe347a8b7a9159cda8c1865a168d91
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2024-12-22 21:56:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-$(document).ready(function() {
-    $('.carousel').slick({
-        dots: false,            // No dots for navigation
-        arrows: false,           // Enable arrows for navigation
-        infinite: false,        // Disable infinite looping
-        centerMode: false,      // No centering
-        slidesToShow: 3,        // Adjust the number of visible items
-        slidesToScroll: 1,      // Number of items to scroll at a time
-        prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
-        nextArrow: "<button type='button' class='mission-next-arrow'><i class='fa fa-3x fa-chevron-circle-right'></i></button>"
-    });
+// Slick
+$('.carousel').slick({
+    slidesToShow: 3,
+    dots: true,
+});
 
-    // Toggle header menu on click
-    $(".menuToggler").click(function() {
-        $(this).toggleClass("active");
-        $("#header").toggleClass("active"); 
-    });
+// Swiper
+const swiper = new Swiper('.carousel', {
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
