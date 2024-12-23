9b1bcbaaf266dbaf58d263ea6ac672b119d0c0c4
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2024-12-22 21:17:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-$( document ).ready(function() {
-    $('.box-carousel').slick({
-     dots: false,
-     arrows: false,
-     infinite: false,
-     centerMode: false,
-     slidesToShow: 20,
-     prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
-     nextArrow: "<button type='button' class='mission-next-arrow'><i class='fa fa-3x fa-chevron-circle-right'></i></button>"
- });
+$(document).ready(function() {
+    $('.carousel').slick({
+        dots: false,            // No dots for navigation
+        arrows: true,           // Enable arrows for navigation
+        infinite: false,        // Disable infinite looping
+        centerMode: false,      // No centering
+        slidesToShow: 3,        // Adjust the number of visible items
+        slidesToScroll: 1,      // Number of items to scroll at a time
+        prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
+        nextArrow: "<button type='button' class='mission-next-arrow'><i class='fa fa-3x fa-chevron-circle-right'></i></button>"
+    });
 
- $(".menuToggler").click(function(){
-     $(this).toggleClass("active");
-     $("#header").toggleClass("active"); 
- });
-
-
-
+    // Toggle header menu on click
+    $(".menuToggler").click(function() {
+        $(this).toggleClass("active");
+        $("#header").toggleClass("active"); 
+    });
 });
