b9a3c8440f82894ff67fa00e97426a25821f3e84
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2024-12-22 21:40:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('.carousel').slick({
         dots: false,            // No dots for navigation
-        arrows: true,           // Enable arrows for navigation
+        arrows: false,           // Enable arrows for navigation
         infinite: false,        // Disable infinite looping
-        centerMode: true,      // No centering
+        centerMode: false,      // No centering
         slidesToShow: 3,        // Adjust the number of visible items
         slidesToScroll: 1,      // Number of items to scroll at a time
         prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
