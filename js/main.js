17751f65c8981b86032b6b4e88cf8f6c23d3aba9
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2025-05-19 21:24:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-// Slick
-$('.carousel').slick({
-    slidesToShow: 3,
-    dots: true,
-});
 
-// Swiper
-const swiper = new Swiper('.carousel', {
-    slidesPerView: 3,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
